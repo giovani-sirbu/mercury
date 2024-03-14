e13055b7b8d35117005347662c4bb1a841689ba3
Giovani Sirbu <gs@w3solutions.ro>
2024-03-14 18:18:28 +0200

    wip: api response;

--- adaptors/gin/init.go
+++
@@ -1,9 +0,0 @@
-package ginAdaptors
-
-import "github.com/gin-gonic/gin"
-
-func Init(c *gin.Context) {
-	gin.Default()
-
-	c.Next()
-}
--- adaptors/gin/response.go
+++ adaptors/gin/response.go
@@ -1,7 +1,11 @@
 package ginAdaptors
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"net/http"
+	"strings"
 )
 
 type Error struct {
@@ -9,9 +13,45 @@ type Error struct {
 	Error string `json:"error"`
 }
 
+type ValidationErrors struct {
+	Message string      `json:"message"`
+	Errors  interface{} `json:"errors"`
+}
+
 type Data struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
+}
+
+type Errors map[string][]string
+
+func (e Errors) Error() string {
+	return "VALIDATION_ERROR"
+}
+
+func checkTagRules(e validator.FieldError) (errMsg string) {
+	tag, field, param, value := e.ActualTag(), e.Field(), e.Param(), e.Value()
+
+	switch tag {
+	case "required":
+		errMsg = "This field is required"
+		break
+	case "email":
+		errMsg = fmt.Sprintf("%q is not a valid email", value)
+		break
+	case "min":
+		errMsg = fmt.Sprintf("%s must have length greater than %v chars", field, param)
+	case "max":
+		errMsg = fmt.Sprintf("%s must have length less than %v chars", field, param)
+		break
+	case "containsany":
+		errMsg = fmt.Sprintf("%s must contain at least one of the following chars: %v", field, param)
+		break
+	default:
+		errMsg = "failed to validate field"
+	}
+
+	return
 }
 
 func Response(c *gin.Context, statusCode int, data interface{}) error {
@@ -31,4 +71,27 @@ func ErrorResponse(c *gin.Context, statusCode int, message string) error {
 		Code:  statusCode,
 		Error: message,
 	})
+}
+
+func ValidationResponse(c *gin.Context, _err error) {
+	errors := Errors{}
+
+	switch err := _err.(type) {
+	case validator.ValidationErrors:
+		for _, e := range err {
+			field := e.Field()
+			msg := checkTagRules(e)
+			errors[strings.ToLower(field)] = append(errors[field], msg)
+		}
+	default:
+		errors["non_field_error"] = append(errors["non_field_error"], err.Error())
+	}
+
+	err := Response(c, http.StatusUnprocessableEntity, ValidationErrors{
+		Message: errors.Error(),
+		Errors:  errors,
+	})
+	if err != nil {
+		return
+	}
 }
