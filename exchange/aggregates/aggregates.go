860621f84f4eb67ef1209d702bc8e9515c49c10a
Giovani Sirbu <gs@w3solutions.ro>
2024-04-09 23:55:03 +0300

    update exchange structure;

--- exchange/aggregates/aggregates.go
+++ exchange/aggregates/aggregates.go
@@ -3,10 +3,12 @@ package aggregates
 type (
 	// Exchange structure to initialize an exchange
 	Exchange struct {
-		Name      string `bson:"name" json:"name"`
-		ApiKey    string `bson:"apiKey" json:"apiKey"`
-		ApiSecret string `bson:"apiSecret" json:"apiSecret"`
-		TestNet   bool   `bson:"testNet" json:"testNet"`
+		ID        uint   `gorm:"primaryKey" form:"id" json:"id" xml:"id"`
+		Label     string `gorm:"type:varchar(50)" bson:"label" json:"label" form:"label" xml:"label" validate:"required,min=3,max=50"`
+		Name      string `gorm:"type:varchar(50)" bson:"name" json:"name" form:"name" xml:"name" validate:"required,min=3,max=50"`
+		ApiKey    string `gorm:"type:varchar(200)" bson:"apiKey" json:"apiKey" form:"apiKey" xml:"apiKey" validate:"omitempty,min=10,max=150"`
+		ApiSecret string `gorm:"type:varchar(200)" bson:"apiSecret" json:"apiSecret" form:"apiSecret" xml:"apiSecret" validate:"omitempty,min=10,max=150"`
+		TestNet   bool   `gorm:"type:boolean;default:false" bson:"testNet" json:"testNet" form:"testNet" xml:"testNet"`
 	}
 	// CreateOrderResponse create order structure
 	CreateOrderResponse struct {
