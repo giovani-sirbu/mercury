879b9739346e663896ad8ae1a41ef0b46bf3f8f2
Giovani Sirbu <gs@w3solutions.ro>
2024-05-15 18:26:06 +0300

    remove: exchange id from trades exchange;

--- trades/aggragates/trades_exchanges.go
+++ trades/aggragates/trades_exchanges.go
@@ -6,14 +6,13 @@ import (
 )
 
 type TradesExchanges struct {
-	ID         uint           `gorm:"primaryKey" form:"id" json:"id" xml:"id"`
-	ExchangeID uint           `form:"exchangeId" json:"exchangeId" xml:"exchangeId"`
-	UserID     uint           `form:"userId" json:"userId" xml:"userId"`
-	Name       string         `gorm:"type:varchar(50)" bson:"name" json:"name" form:"name" xml:"name" validate:"required,min=3,max=50"`
-	ApiKey     string         `gorm:"type:varchar(200)" bson:"apiKey" json:"apiKey" form:"apiKey" xml:"apiKey" validate:"required,min=10,max=150"`
-	ApiSecret  string         `gorm:"type:varchar(200)" bson:"apiSecret" json:"apiSecret" form:"apiSecret" xml:"apiSecret" validate:"required,min=10,max=150"`
-	TestNet    bool           `gorm:"type:boolean;default:false" bson:"testNet" json:"testNet" form:"testNet" xml:"testNet"`
-	CreatedAt  time.Time      `form:"createdAt" json:"-" xml:"createdAt"`
-	UpdatedAt  time.Time      `form:"updatedAt" json:"-" xml:"updatedAt"`
-	DeletedAt  gorm.DeletedAt `form:"deletedAt" json:"-" xml:"deletedAt"`
+	ID        uint           `gorm:"primaryKey" form:"id" json:"id" xml:"id"`
+	UserID    uint           `form:"userId" json:"userId" xml:"userId"`
+	Name      string         `gorm:"type:varchar(50)" bson:"name" json:"name" form:"name" xml:"name" validate:"required,min=3,max=50"`
+	ApiKey    string         `gorm:"type:varchar(200)" bson:"apiKey" json:"apiKey" form:"apiKey" xml:"apiKey" validate:"required,min=10,max=150"`
+	ApiSecret string         `gorm:"type:varchar(200)" bson:"apiSecret" json:"apiSecret" form:"apiSecret" xml:"apiSecret" validate:"required,min=10,max=150"`
+	TestNet   bool           `gorm:"type:boolean;default:false" bson:"testNet" json:"testNet" form:"testNet" xml:"testNet"`
+	CreatedAt time.Time      `form:"createdAt" json:"-" xml:"createdAt"`
+	UpdatedAt time.Time      `form:"updatedAt" json:"-" xml:"updatedAt"`
+	DeletedAt gorm.DeletedAt `form:"deletedAt" json:"-" xml:"deletedAt"`
 }
