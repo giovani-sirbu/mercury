3403333a0a59fb8803e9280a51e84d9c2339263b
Giovani Sirbu <gs@w3solutions.ro>
2024-05-15 16:38:24 +0300

    update: trades exchanges struct;

--- trades/aggragates/trades_exchanges.go
+++ trades/aggragates/trades_exchanges.go
@@ -9,6 +9,7 @@ type TradesExchanges struct {
 	ID         uint           `gorm:"primaryKey" form:"id" json:"id" xml:"id"`
 	ExchangeID uint           `form:"exchangeId" json:"exchangeId" xml:"exchangeId"`
 	UserID     uint           `form:"userId" json:"userId" xml:"userId"`
+	Name       string         `gorm:"type:varchar(50)" bson:"name" json:"name" form:"name" xml:"name" validate:"required,min=3,max=50"`
 	ApiKey     string         `gorm:"type:varchar(200)" bson:"apiKey" json:"apiKey" form:"apiKey" xml:"apiKey" validate:"required,min=10,max=150"`
 	ApiSecret  string         `gorm:"type:varchar(200)" bson:"apiSecret" json:"apiSecret" form:"apiSecret" xml:"apiSecret" validate:"required,min=10,max=150"`
 	TestNet    bool           `gorm:"type:boolean;default:false" bson:"testNet" json:"testNet" form:"testNet" xml:"testNet"`
