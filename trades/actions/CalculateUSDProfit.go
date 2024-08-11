f802be7ecfdc0b477df0de9ddaf7d942d8e956cc
Giovani Sirbu <gs@w3solutions.ro>
2024-08-11 16:35:19 +0300

    feat: add CalculateProfit function; refactor: CalculateUSDProfit;

--- trades/actions/CalculateUSDProfit.go
+++ trades/actions/CalculateUSDProfit.go
@@ -1,25 +1,46 @@
 package actions
 
 import (
-	"fmt"
-	"github.com/giovani-sirbu/mercury/exchange/aggregates"
+	"github.com/giovani-sirbu/mercury/crypto"
+	"github.com/giovani-sirbu/mercury/exchange"
+	"github.com/giovani-sirbu/mercury/log"
+	"github.com/giovani-sirbu/mercury/trades/aggragates"
+	"os"
 	"strings"
 )
 
 const fiatConversionSymbol = "USDT"
 
-func CalculateUSDProfit(client aggregates.Actions, profit float64, profitAsset string) float64 {
-	if strings.Contains(profitAsset, "USD") {
-		return profit
-	}
-
-	// calculate usd profit
-	price, err := client.GetPrice(profitAsset + "/" + fiatConversionSymbol)
-
-	if err != nil {
-		fmt.Println("could not calculate price")
+func CalculateUSDProfit(trade aggragates.Trades) float64 {
+	secretKey, decryptErr := crypto.Decrypt(trade.Exchange.ApiSecret, os.Getenv("API_SECRET"))
+	if decryptErr != nil {
+		log.Error(decryptErr.Error(), "decryptErr", "CalculateUSDProfit")
 		return 0
 	}
 
-	return profit * price
+	exchangeInit := exchange.Exchange{
+		Name:      trade.Exchange.Name,
+		ApiSecret: secretKey,
+		ApiKey:    trade.Exchange.ApiKey,
+		TestNet:   trade.Exchange.TestNet,
+	}
+	client, clientErr := exchangeInit.Client()
+	if clientErr != nil {
+		log.Error(clientErr.Error(), "clientErr", "CalculateUSDProfit")
+		return 0
+	}
+
+	// return result if profit asset is in usd
+	if strings.Contains(trade.ProfitAsset, "USD") {
+		return trade.Profit
+	}
+
+	// get profit asset price
+	price, priceErr := client.GetPrice(trade.ProfitAsset + "/" + fiatConversionSymbol)
+	if priceErr != nil {
+		log.Error(priceErr.Error(), "priceErr", "CalculateUSDProfit")
+		return 0
+	}
+
+	return trade.Profit * price
 }
