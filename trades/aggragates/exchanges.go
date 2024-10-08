2cbd066bcb31d4b24531b208acd25a2c03e27d76
Giovani Sirbu <gs@w3solutions.ro>
2024-10-08 16:06:39 +0300

    add Exchanges schema;

---
+++ trades/aggragates/exchanges.go
@@ -0,0 +1,16 @@
+package aggragates
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Exchanges struct {
+	ID        uint           `gorm:"primaryKey" form:"id" json:"id" xml:"id"`
+	Name      string         `gorm:"type:varchar(50)" bson:"name" json:"name" form:"name" xml:"name" validate:"required,min=3,max=50"`
+	Icon      string         `gorm:"type:varchar(200)" bson:"icon" json:"icon" form:"icon" xml:"icon" validate:"required"`
+	Status    Status         `gorm:"type:varchar(50);default:active" bson:"status" json:"status" form:"status" xml:"status"`
+	CreatedAt time.Time      `form:"createdAt" json:"-" xml:"createdAt"`
+	UpdatedAt time.Time      `form:"updatedAt" json:"-" xml:"updatedAt"`
+	DeletedAt gorm.DeletedAt `form:"deletedAt" json:"-" xml:"deletedAt"`
+}
