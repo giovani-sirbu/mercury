c6b875e63e8cb60f506c3a6320a14f5c8f3e80c3
Giovani Sirbu <gs@w3solutions.ro>
2024-10-10 22:48:53 +0300

    wip;

--- trades/aggragates/exchanges.go
+++ trades/aggragates/exchanges.go
@@ -6,11 +6,12 @@ import (
 )
 
 type Exchanges struct {
-	ID        uint           `gorm:"primaryKey" form:"id" json:"id" xml:"id"`
-	Name      string         `gorm:"type:varchar(50)" bson:"name" json:"name" form:"name" xml:"name" validate:"required,min=3,max=50"`
-	Icon      string         `gorm:"type:varchar(200)" bson:"icon" json:"icon" form:"icon" xml:"icon" validate:"required"`
-	Status    Status         `gorm:"type:varchar(50);default:active" bson:"status" json:"status" form:"status" xml:"status"`
-	CreatedAt time.Time      `form:"createdAt" json:"-" xml:"createdAt"`
-	UpdatedAt time.Time      `form:"updatedAt" json:"-" xml:"updatedAt"`
-	DeletedAt gorm.DeletedAt `form:"deletedAt" json:"-" xml:"deletedAt"`
+	ID          uint           `gorm:"primaryKey" form:"id" json:"id" xml:"id"`
+	Name        string         `gorm:"type:varchar(50)" bson:"name" json:"name" form:"name" xml:"name" validate:"required,min=3,max=50"`
+	Icon        string         `gorm:"type:varchar(200)" bson:"icon" json:"icon" form:"icon" xml:"icon" validate:"required"`
+	ApiTutorial string         `gorm:"type:varchar(500)" bson:"apiTutorial" json:"apiTutorial" form:"apiTutorial" xml:"apiTutorial" validate:"required"`
+	Status      Status         `gorm:"type:varchar(50);default:active" bson:"status" json:"status" form:"status" xml:"status"`
+	CreatedAt   time.Time      `form:"createdAt" json:"-" xml:"createdAt"`
+	UpdatedAt   time.Time      `form:"updatedAt" json:"-" xml:"updatedAt"`
+	DeletedAt   gorm.DeletedAt `form:"deletedAt" json:"-" xml:"deletedAt"`
 }
--- trades/aggragates/trades.go
+++ trades/aggragates/trades.go
@@ -7,26 +7,26 @@ import (
 
 type (
 	Trades struct {
-		ID              uint            `gorm:"primaryKey" form:"id" json:"id" xml:"id"`
-		UserID          uint            `gorm:"index:idx_dashboard_stats,priority:1" form:"userId" json:"userId" xml:"userId"`
-		ParentID        uint            `gorm:"index" form:"parentId" json:"parentId" xml:"parentId"`
-		Symbol          string          `gorm:"type:varchar(10); index" bson:"symbol" json:"symbol"`
-		ParentSymbol    string          `gorm:"type:varchar(10); index" bson:"parentSymbol" json:"parentSymbol"`
-		PositionType    string          `gorm:"type:varchar(50); default:new" bson:"positionType" json:"positionType"`
-		PositionPrice   float64         `bson:"positionPrice" json:"positionPrice"`
-		ExchangeID      int             `gorm:"index:idx_dashboard_stats,priority:2" form:"exchangeId" json:"-" xml:"exchangeId"`
-		Exchange        TradesExchanges `form:"exchange" json:"exchange" xml:"exchange"`
-		StrategyID      int             `form:"strategyId" json:"-" xml:"strategyId"`
-		Strategy        Strategies      `gorm:"foreignKey:StrategyID;references:ID"  form:"strategy" json:"strategy" xml:"strategy"`
-		Settings        SettingsPairs   `gorm:"foreignKey:idx_symbol;references:StrategyID"  form:"settings" json:"settings" xml:"settings"`
+		ID            uint            `gorm:"primaryKey" form:"id" json:"id" xml:"id"`
+		UserID        uint            `gorm:"index:idx_dashboard_stats,priority:1" form:"userId" json:"userId" xml:"userId"`
+		ParentID      uint            `gorm:"index" form:"parentId" json:"parentId" xml:"parentId"`
+		Symbol        string          `gorm:"type:varchar(10); index" bson:"symbol" json:"symbol"`
+		ParentSymbol  string          `gorm:"type:varchar(10); index" bson:"parentSymbol" json:"parentSymbol"`
+		PositionType  string          `gorm:"type:varchar(50); default:new" bson:"positionType" json:"positionType"`
+		PositionPrice float64         `bson:"positionPrice" json:"positionPrice"`
+		ExchangeID    int             `gorm:"index:idx_dashboard_stats,priority:2" form:"exchangeId" json:"-" xml:"exchangeId"`
+		Exchange      TradesExchanges `form:"exchange" json:"exchange" xml:"exchange"`
+		StrategyID    int             `form:"strategyId" json:"-" xml:"strategyId"`
+		Strategy      Strategies      `gorm:"foreignKey:StrategyID;references:ID"  form:"strategy" json:"strategy" xml:"strategy"`
+		//Settings        SettingsPairs   `gorm:"foreignKey:idx_symbol;references:StrategyID"  form:"settings" json:"settings" xml:"settings"`
 		USDProfit       float64         `bson:"usdProfit" json:"usdProfit"`
 		Profit          float64         `bson:"profit" json:"profit"`
 		ProfitAsset     string          `bson:"profitAsset" json:"profitAsset"`
 		PreventNewTrade bool            `gorm:"type:boolean;default:false" bson:"preventNewTrade" json:"preventNewTrade"`
 		Inverse         bool            `gorm:"type:boolean;default:false" bson:"inverse" json:"inverse"`
 		PendingOrder    int64           `gorm:"index" bson:"pendingOrder" json:"pendingOrder"`
-		History         []TradesHistory `gorm:"foreignKey:TradeID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" bson:"history" json:"history"`
-		Logs            []TradesLogs    `gorm:"foreignKey:TradeID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" bson:"logs" json:"logs"`
+		History         []TradesHistory `gorm:"foreignKey:TradeID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" bson:"history" json:"history"`
+		Logs            []TradesLogs    `gorm:"foreignKey:TradeID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" bson:"logs" json:"logs"`
 		Status          Status          `gorm:"default:active;index;index:idx_dashboard_stats,priority:3" bson:"status" json:"status"`
 		CreatedAt       time.Time       `form:"createdAt" json:"createdAt" xml:"createdAt"`
 		UpdatedAt       time.Time       `form:"updatedAt;index:idx_dashboard_stats,priority:4" json:"updatedAt" xml:"updatedAt"`
