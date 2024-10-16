c238677f9ab4bea396740b14e08a84e6bd214bd6
Giovani Sirbu <gs@w3solutions.ro>
2024-10-16 13:46:43 +0300

    update app version;

--- trades/aggragates/settings_app_version.go
+++ trades/aggragates/settings_app_version.go
@@ -9,7 +9,7 @@ type AppVersion struct {
 	ID             uint           `gorm:"primaryKey" form:"id" json:"id" xml:"id"`
 	Os             string         `gorm:"type:varchar(50)" form:"os" json:"os" xml:"os"`
 	Version        string         `gorm:"" form:"version" json:"version" xml:"version"`
-	UpdateRequired bool           `gorm:"default:false" bson:"update_required" json:"update_required" form:"update_required" xml:"update_required"`
+	UpdateRequired bool           `gorm:"default:false" bson:"updateRequired" json:"updateRequired" form:"updateRequired" xml:"updateRequired"`
 	Description    string         `gorm:"type:varchar(500)" form:"description" json:"description" xml:"description"`
 	CreatedAt      time.Time      `form:"createdAt" json:"-" xml:"createdAt"`
 	UpdatedAt      time.Time      `form:"updatedAt" json:"-" xml:"updatedAt"`
