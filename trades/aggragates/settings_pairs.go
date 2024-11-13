c46e3449e23c30b56627d2b5a3e76ddf834df54e
Giovani Sirbu <gs@w3solutions.ro>
2024-11-13 22:15:34 +0200

    wip: refactor strategies;

--- trades/aggragates/settings_pairs.go
+++ trades/aggragates/settings_pairs.go
@@ -24,17 +24,14 @@ type (
 		Multiplier         float64 `bson:"multiplier" json:"multiplier"`
 	}
 	SettingsPairs struct {
-		ID           uint               `gorm:"primaryKey" form:"id" json:"id" xml:"id"`
+		ID           uint               `gorm:"primaryKey" form:"id" json:"-" xml:"id"`
 		StrategyID   int                `gorm:"index:idx_symbol_strategy_id,unique" form:"strategyID" json:"-" xml:"strategyID"`
 		Symbol       string             `gorm:"type:varchar(20);index:idx_symbol_strategy_id,unique" bson:"symbol" json:"-" form:"symbol" xml:"symbol" validate:"required,min=3,max=20"`
 		Exchange     string             `gorm:"type:varchar(50);" bson:"exchange" json:"exchange" form:"exchange" xml:"exchange" validate:"required,min=3,max=50"`
-		LotSize      uint8              `bson:"lotSize" json:"lotSize" form:"lotSize" xml:"lotSize" validate:"required"`
-		PriceFilter  uint8              `bson:"priceFilter" json:"priceFilter" form:"priceFilter" xml:"priceFilter" validate:"required"`
-		MinNotional  float64            `bson:"minNotional" json:"minNotional" form:"minNotional" xml:"minNotional" validate:"required"`
 		Rank         uint8              `bson:"rank" json:"rank" form:"rank" xml:"rank" validate:"required"`
 		Status       string             `gorm:"default:active" bson:"status" json:"status"`
 		TradeFilters TradeFilters       `gorm:"type:jsonb;serializer:json;" bson:"tradeFilters" json:"tradeFilters" form:"tradeFilters" xml:"tradeFilters"`
-		Settings     []StrategySettings `gorm:"type:jsonb;serializer:json;" bson:"settings" json:"settings" form:"settings" xml:"settings"`
+		Settings     []StrategySettings `gorm:"type:jsonb;serializer:json;" bson:"settings" json:"-" form:"settings" xml:"settings"`
 		CreatedAt    time.Time          `form:"createdAt" json:"-" xml:"createdAt"`
 		UpdatedAt    time.Time          `form:"updatedAt" json:"-" xml:"updatedAt"`
 		DeletedAt    gorm.DeletedAt     `form:"deletedAt" json:"-" xml:"deletedAt"`
