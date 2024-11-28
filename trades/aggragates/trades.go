6cbf49f68178093dd0c1b6ca33d12b91194abcbf
Giovani Sirbu <gs@w3solutions.ro>
2024-11-28 16:27:25 +0200

    fix;

--- trades/aggragates/trades.go
+++ trades/aggragates/trades.go
@@ -20,7 +20,7 @@ type (
 		ExchangeID      int             `gorm:"index:idx_dashboard_stats,priority:2;constraint:OnDelete:CASCADE;" form:"exchangeId" json:"-" xml:"exchangeId"`
 		Exchange        TradesExchanges `form:"exchange" json:"exchange" xml:"exchange"`
 		ExchangeName    string          `gorm:"type:varchar(50);uniqueIndex:idx_symbol_strategy_id,priority:3;" bson:"exchangeName" json:"-"`
-		StrategyID      int             `gorm:"uniqueIndex:idx_symbol_strategy_id,priority:2;" form:"strategyId" json:"-" xml:"strategyId"`
+		StrategyID      int             `gorm:"uniqueIndex:idx_symbol_strategy_id,priority:2;" form:"strategyId" json:"strategyId" xml:"strategyId"`
 		Strategy        Strategies      `gorm:"foreignKey:StrategyID;references:ID"  form:"strategyInfo" json:"strategyInfo" xml:"strategyInfo"`
 		StrategyPair    StrategiesPairs `gorm:"foreignKey:Symbol,StrategyID,ExchangeName;references:Symbol,StrategyID,Exchange" json:"strategyPair"`
 		USDProfit       float64         `bson:"usdProfit" json:"usdProfit"`
