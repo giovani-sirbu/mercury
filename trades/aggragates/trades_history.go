8ff2ac63eb86e8ed92cf2c4a9403876bedc0cf93
Giovani Sirbu <gs@w3solutions.ro>
2024-12-03 11:04:17 +0200

    refactor: trades fees;

--- trades/aggragates/trades_history.go
+++ trades/aggragates/trades_history.go
@@ -5,14 +5,14 @@ import (
 )
 
 type TradesHistory struct {
-	ID        uint                `gorm:"primaryKey" form:"id" json:"id" xml:"id"`
-	TradeID   uint                `gorm:"uniqueIndex:idx_trade_id_order_id,priority:1" form:"tradeId" json:"tradeId" xml:"tradeId"`
-	Type      string              `bson:"type" json:"type"`
-	Quantity  float64             `bson:"quantity" json:"quantity"`
-	Price     float64             `bson:"price" json:"price"`
-	Fees      []TradesHistoryFees `gorm:"foreignKey:HistoryID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"  bson:"fees" json:"fees"`
-	OrderId   int64               `gorm:"uniqueIndex:idx_trade_id_order_id,priority:2" bson:"orderId" json:"orderId"`
-	Status    string              `bson:"status" json:"status"`
-	CreatedAt time.Time           `json:"createdAt"`
-	UpdatedAt time.Time           `json:"updatedAt"`
+	ID        uint         `gorm:"primaryKey" form:"id" json:"id" xml:"id"`
+	TradeID   uint         `gorm:"uniqueIndex:idx_trade_id_order_id,priority:1" form:"tradeId" json:"tradeId" xml:"tradeId"`
+	Type      string       `bson:"type" json:"type"`
+	Quantity  float64      `bson:"quantity" json:"quantity"`
+	Price     float64      `bson:"price" json:"price"`
+	Fees      []TradesFees `gorm:"foreignKey:HistoryID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"  bson:"fees" json:"fees"`
+	OrderId   int64        `gorm:"uniqueIndex:idx_trade_id_order_id,priority:2" bson:"orderId" json:"orderId"`
+	Status    string       `bson:"status" json:"status"`
+	CreatedAt time.Time    `json:"createdAt"`
+	UpdatedAt time.Time    `json:"updatedAt"`
 }
