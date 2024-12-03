1104644c3684e0fcdbc6831b313b8ff7e069201d
ice-castor <cosmin.turcin@leftclick.io>
2024-12-03 17:10:27 +0200

    feature: add min profit on has profit action

---
+++ trades/actions/calculateMinProfit.go
@@ -0,0 +1,11 @@
+package actions
+
+import "github.com/giovani-sirbu/mercury/trades/aggragates"
+
+func CalculateMinProfit(trade aggragates.Trades) float64 {
+	profit := trade.StrategyPair.TradeFilters.MinNotional * (trade.StrategyPair.StrategySettings[0].Percentage / 100)
+	if trade.Inverse {
+		profit *= trade.PositionPrice
+	}
+	return profit
+}
--- trades/actions/hasProfit.go
+++ trades/actions/hasProfit.go
@@ -35,7 +35,7 @@ func HasProfit(event events.Events) (events.Events, error) {
 
 	event.Trade.Profit = profit
 
-	if profit-fee < 0 {
+	if profit-fee < CalculateMinProfit(event.Trade) {
 		msg := fmt.Sprintf("profit: %f is smaller then min profit for symbol %s, trade id %d, user id %d", profit-fee, event.Trade.Symbol, event.Trade.ID, event.Trade.UserID)
 		return event, fmt.Errorf(msg)
 	}
