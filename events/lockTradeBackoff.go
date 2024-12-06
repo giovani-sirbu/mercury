81970f6596ad30c37370ec46b58b0f6f00e739f8
ice-castor <cosmin.turcin@leftclick.io>
2024-12-06 10:55:46 +0200

    fix: add mutex locker on backoff lock trade

--- events/lockTradeBackoff.go
+++ events/lockTradeBackoff.go
@@ -2,10 +2,12 @@ package events
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 var backoffTries = make(map[uint]time.Duration)
+var rwLocker sync.RWMutex
 
 const startingBackOff = 1 * time.Second
 const maxBackOff = 60 * time.Second
@@ -22,11 +24,15 @@ func (e Events) LockTradeWithBackOff() {
 	if backoffTries[e.Trade.ID] < startingBackOff {
 		lockDuration = startingBackOff
 	} else {
+		rwLocker.RLock()
 		lockDuration = backoffTries[e.Trade.ID] * 2
+		rwLocker.RUnlock()
 		if lockDuration > maxBackOff {
 			lockDuration = maxBackOff
 		}
 	}
+	rwLocker.Lock()
 	backoffTries[e.Trade.ID] = lockDuration
+	rwLocker.Unlock()
 	e.LockTrade(lockDuration)
 }
