0b20d56cc4c05a7b93a520d28ebf2df755b313db
Giovani Sirbu <gs@w3solutions.ro>
2025-03-19 10:32:49 +0200

    wip: refactor events

--- events/events.go
+++ events/events.go
@@ -25,12 +25,12 @@ type (
 
 // Next Function to run the next event if we have multiple events
 func (e Events) Next() error {
-	if len(e.EventsNames) == 1 {
+	if len(e.EventsNames) <= 1 {
 		return nil
 	}
+
 	e.EventsNames = e.EventsNames[1:]
-	err := e.Run()
-	return err
+	return e.Run()
 }
 
 // Run Function to run events
@@ -38,34 +38,23 @@ func (e Events) Run() error {
 	if len(e.EventsNames) == 0 {
 		return nil
 	}
-	if e.Events[e.EventsNames[0]] == nil {
+
+	currentEvent := e.EventsNames[0]
+	eventFunc, exists := e.Events[currentEvent]
+	if !exists || eventFunc == nil {
 		return nil
 	}
 
-	newEvent, err := e.Events[e.EventsNames[0]](e)
+	newEvent, err := eventFunc(e)
 	if err != nil {
 		e.LockTradeWithBackOff()
-		pairSymbols := strings.Split(e.Trade.Symbol, "/")
-		assetSymbol := pairSymbols[1]
-		msg := fmt.Sprintf("%s | User ID: #%d | Trade Info: (ID: #%d, Position Type: %s, Position Price: %f, Impasse: %t, Profit: %f, Depths: %d, Inverse used: %f)",
-			err.Error(),
-			e.Trade.UserID,
-			e.Trade.ID,
-			e.Trade.PositionType,
-			e.Trade.PositionPrice,
-			e.Trade.Inverse,
-			e.Params.Profit,
-			len(e.Trade.History),
-			aggragates.FindUsedAmount(e.Params.InverseUsedAmount, assetSymbol),
-		)
-		log.Error(msg, "Run events", "")
-		return err
+		return e.logEventError(err)
 	}
-	err = newEvent.Next()
-	if err != nil {
-		e.LockTradeWithBackOff()
-	}
-	return err
+
+	// Cleanup goroutine to remove the entry after lockDuration expires
+	delete(backoffTries, e.Trade.ID)
+
+	return newEvent.Next()
 }
 
 // Add Function to register a new event or replace a default one
@@ -77,4 +66,26 @@ func (e Events) Add(event string, action func(Events) (Events, error)) Events {
 	newEvent[event] = action
 	e.Events = newEvent
 	return e
+}
+
+// logEventError formats and logs event execution errors
+func (e Events) logEventError(err error) error {
+	pairSymbols := strings.Split(e.Trade.Symbol, "/")
+	assetSymbol := pairSymbols[1]
+
+	msg := fmt.Sprintf(
+		"%s | User ID: #%d | Trade Info: (ID: #%d, Position Type: %s, Position Price: %f, Impasse: %t, Profit: %f, Depths: %d, Inverse used: %f)",
+		err.Error(),
+		e.Trade.UserID,
+		e.Trade.ID,
+		e.Trade.PositionType,
+		e.Trade.PositionPrice,
+		e.Trade.Inverse,
+		e.Params.Profit,
+		len(e.Trade.History),
+		aggragates.FindUsedAmount(e.Params.InverseUsedAmount, assetSymbol),
+	)
+
+	log.Error(msg, "Run events", "")
+	return err
 }
