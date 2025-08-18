3e1480936c9b99eefeb1b25ed896649175569d2d
ice-castor <cosmin.turcin@leftclick.io>
2025-08-18 22:54:28 +0300

    fix: add default spot on strategies trade type

--- trades/aggragates/strategies.go
+++ trades/aggragates/strategies.go
@@ -4,7 +4,7 @@ type (
 	Strategies struct {
 		ID        uint           `gorm:"primaryKey" form:"id" json:"id" xml:"id"`
 		Name      string         `gorm:"type:varchar(50)" bson:"name" json:"name" form:"name" xml:"name" validate:"required,min=3,max=50"`
-		TradeType TradeTypes     `gorm:"type:varchar(50)" bson:"tradeType" json:"tradeType" form:"tradeType" xml:"tradeType"`
+		TradeType TradeTypes     `gorm:"type:varchar(50); default:spot" bson:"tradeType" json:"tradeType" form:"tradeType" xml:"tradeType"`
 		Params    StrategyParams `gorm:"type:jsonb;serializer:json;" bson:"params" json:"params" form:"params" xml:"params"`
 	}
 	StrategyParams struct {
